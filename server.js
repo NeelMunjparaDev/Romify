425f012221cad5bae7ece14784bba0a710618bf7
NeelMunjparaDev <19bic503@nirmauni.ac.in>
2024-01-21 20:23:33 +0530

    back to previous

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongooes
   })
   .then(() => console.log('DB connection successful'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on ${port}....`);
 });
